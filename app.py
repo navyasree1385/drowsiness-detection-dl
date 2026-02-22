3a5c91e9f0525a1e6fff5f7908167e72bb4d3994
navyasree1385 <mnsd1385@gmail.com>
2026-02-22 17:05:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,28 @@ import os
 
 app = Flask(__name__)
 
-# Load Model
-model = tf.keras.models.load_model("model/drowsiness_model.h5", compile=False,
-safe_mode=False)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+model = Sequential([
+
+    Conv2D(32,(3,3),activation="relu",input_shape=(64,64,3)),
+    MaxPooling2D(2,2),
+
+    Conv2D(64,(3,3),activation="relu"),
+    MaxPooling2D(2,2),
+
+    Conv2D(128,(3,3),activation="relu"),
+    MaxPooling2D(2,2),
+
+    Flatten(),
+
+    Dense(128,activation="relu"),
+    Dropout(0.5),
+
+    Dense(1,activation="sigmoid")
+])
+model.load_weights("model/drowsiness_weights.h5")
 
 # Haar Cascades
 face_cascade = cv2.CascadeClassifier(
